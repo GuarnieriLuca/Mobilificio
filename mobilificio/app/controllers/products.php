a330a525f62b1abde4f34c540755a0e20adccae4
Kommii <73954641+Kommii@users.noreply.github.com>
2022-05-24 16:40:49 +0200

    Inserimento sito modificato
    Inserimento sito modificato utilizzando il modello mvc

---
+++ mobilificio/app/controllers/products.php
@@ -0,0 +1,9 @@
+<?php
+require '../app/models/products.php';
+
+function controller_products_all()
+{
+    global $data;
+    $data['rows'] = model_products_all();
+    view_render_html();
+}
---
+++ mobilificio/app/models/products.php
@@ -0,0 +1,25 @@
+<?php
+include_once("../app/config.php");
+
+function db_connect()
+{
+  $conn = new mysqli(SERVER, USER, PASS, DB);
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  return $conn;
+}
+
+function model_products_all()
+{
+  $conn = db_connect();
+  $sql = "SELECT * FROM prodotto ORDER BY idProdotto";
+  $result = $conn->query($sql);
+  $rows = $result->fetch_all(MYSQLI_ASSOC);
+  $result->free();
+  $conn->close();
+  return $rows;
+}
+
+?>
